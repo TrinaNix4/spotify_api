842dbc713867ec97f8866c8d0521930102b5fd7c
Trina Nixon <TrinaNixon7@gmail.com>
2023-01-30 19:46:58 -0700

    added .get with json

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 3000;
+const PORT = 8000;
 
 //initialize express, cheerio, axios
 const express = require("express");
@@ -8,5 +8,17 @@ const cheerio = require("cheerio");
 //calling and saving it as app
 const app = express();
 
+app.get("/", (req, res) => {
+  res.json("Wrselcome to my climate change news API");
+});
+
+app.get("/news", (req, res) => {
+  axios
+    .get("https://www.theguardian.com/environment/climate-crisis")
+    .then((response) => {
+      const html = response.data;
+      console.log(html);
+    });
+});
 //get a message to show that everything is running on server
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
